5f18c2c8a7b6e9c26eeaaa6e8d4ce85c3b7f996b
Ben Bowen <bensbow@hotmail.com>
2019-01-01 23:40:05 +0000

    initial commit

---
+++ WindowsFormsApp1/WindowsFormsApp1/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            ViewForm viewForm = new ViewForm();
+            viewForm.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            EntryForm entryForm = new EntryForm();
+            entryForm.Show();
+
+        }
+    }
+}
